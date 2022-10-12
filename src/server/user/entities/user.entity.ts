471d0a2676c4fcc8be5f6c66819ea0ca6632cd55
윤지원 <jayyoon@yunjiwon-ui-MacBookPro-2.local>
2022-10-12 15:58:19 -0400

    Created Initial User Entity

---
+++ src/server/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Double, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  address: string;
+
+  @Column({ type: 'double' })
+  balance: Double;
+}
